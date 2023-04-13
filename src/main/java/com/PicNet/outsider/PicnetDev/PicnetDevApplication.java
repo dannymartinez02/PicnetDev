05a88dbee0a4074c99d15a2540215424c26fd2eb
dannymartinez02 <annydmartinez79@gmail.com>
2023-04-13 17:36:22 -0600

    Initial commit

---
+++ src/main/java/com/PicNet/outsider/PicnetDev/PicnetDevApplication.java
@@ -0,0 +1,13 @@
+package com.PicNet.outsider.PicnetDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicnetDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicnetDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PicNet/outsider/PicnetDev/PicnetDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicNet.outsider.PicnetDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicnetDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
